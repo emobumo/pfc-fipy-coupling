86e31a9a7cc54fed99f2c84009fa4fd68825c0f2
emobumo <834547341@qq.com>
2026-04-07 20:32:57 +0800

    fix Python 2.7 FiPy compatibility and pass smoke test

--- src/fipy_adapter/mesh_init.py
+++ src/fipy_adapter/mesh_init.py
@@ -1,11 +1,12 @@
+# -*- coding: utf-8 -*-
 from fipy import Grid2D
 
 
 def build_mesh(nx=25, ny=15, dx=0.1, dy=0.1):
     mesh = Grid2D(nx=nx, ny=ny, dx=dx, dy=dy)
 
-    x = mesh.cellCenters()[0].value
-    y = mesh.cellCenters()[1].value
+    x = mesh.cellCenters()[0]
+    y = mesh.cellCenters()[1]
     fx, fy = mesh.faceCenters()
 
     return mesh, x, y, fx, fy
--- src/pfc_adapter/particle_writer.py
+++ src/pfc_adapter/particle_writer.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import itasca.ball as balls
 from scipy.interpolate import Rbf
 
